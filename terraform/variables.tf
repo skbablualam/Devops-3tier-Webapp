d93c23fae584411bcb0dddf0bf7118fc6c57d752
Bablu Alam <52425424+skbablualam@users.noreply.github.com>
2025-04-17 10:03:39 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,18 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
 
+variable "ami" {
+  description = "AMI ID for EC2"
+  default     = "ami-0c55b159cbfafe1f0" # Update with valid AMI for your region
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "SSH Key name to access EC2"
+}
