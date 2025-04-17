5bbc867ee201b1e0d60312027462581823e7eebe
Bablu Alam <52425424+skbablualam@users.noreply.github.com>
2025-04-17 10:02:02 +0530

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE IF NOT EXISTS sampledb;
 
+USE sampledb;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100)
+);
+
+INSERT INTO users (name, email) VALUES ('Bablu', 'bablu@example.com');
