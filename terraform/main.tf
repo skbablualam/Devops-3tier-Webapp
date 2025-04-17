f387c238833546825d484e437b09954c2e11d277
Bablu Alam <52425424+skbablualam@users.noreply.github.com>
2025-04-17 10:03:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = var.region
+}
 
+resource "aws_instance" "web_server" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "devops-3tier-instance"
+  }
+}
